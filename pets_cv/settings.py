e0c9c8549c386017e556c3814e30c3d63fb8403e
Maxigong <maxi@nolgong.com>
2022-03-12 18:07:05 +0900

    STATICFILES_DIRS

--- pets_cv/settings.py
+++ pets_cv/settings.py
@@ -113,9 +113,13 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
